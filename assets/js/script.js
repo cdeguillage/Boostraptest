822ec7eccb1e420b69be6adca0026fb2767a4125
cdeguillage <cdeguillage@free.fr>
2018-10-15 16:23:54 +0200

    Trop cool mon minuteur d'oeufs à la coque

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,27 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 
-
 });
+
+
+var maChaine = document.getElementById("maChaine").getAttribute("data-affiche");
+var maChaineDecompose = maChaine.split("");
+var ChaineAAfficher = "";
+var timeBoucle;
+
+function boucle() {
+
+    if (maChaineDecompose.length > 0)
+    {
+        ChaineAAfficher += maChaineDecompose.shift();
+        document.getElementById("maChaine").innerHTML = ChaineAAfficher;
+    }
+    else
+    {
+        clearTimeout(timeBoucle);
+    }
+
+    timeBoucle = setTimeout('boucle(ChaineAAfficher)', 170);
+}
+
+boucle(ChaineAAfficher);
